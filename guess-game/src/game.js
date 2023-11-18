096fb73c08ab64e71a07c401d484d3c32231d02a
ertugrulsak <ertugrulsak33@gmail.com>
2023-11-18 17:52:00 +0300

    added(guess-game): game file

---
+++ guess-game/src/game.js
@@ -0,0 +1,12 @@
+import {useState} from "react";
+
+function game () {
+
+    const[user, setUser]= useState();
+
+        return(
+            <div>Hoşgeldiniz</div>
+        )
+    }
+
+    export default game;
