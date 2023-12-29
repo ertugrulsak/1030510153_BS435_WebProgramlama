13c4e6de00615c4615db40cc4e673ab3c71f719a
ertugrulsak <ertugrulsak33@gmail.com>
2023-12-29 14:20:31 +0300

    fixed(guess-game): game structure

--- guess-game/src/App.js
+++ guess-game/src/App.js
@@ -1,19 +1,63 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./game.css";
 
 function App() {
+    const [randomNumber, setRandomNumber] = useState(generateRandomNumber());
+    const [userInput, setUserInput] = useState("");
+    const [message, setMessage] = useState("");
+    const [test , setTest] = useState(1);
 
-    const [randomNumber, setRandomNumber] = useState(0);
+    function generateRandomNumber() {
+        return Math.floor(Math.random() * 100) + 1;
+    }
 
-    const generateRandomNumber = () => {
-        const randomNum = Math.floor(Math.random() * 100) + 1;
-        setRandomNumber(randomNum);
+    const handleInputChange = (event) => {
+        setUserInput(event.target.value);
+    };
+
+    const handleGuess = () => {
+        const userGuess = parseInt(userInput, 10);
+
+        if (isNaN(userGuess)) {
+            setMessage("Geçerli bir sayı girin.");
+        } else {
+            setTest((prevTest) => prevTest + 1);
+
+            if (userGuess === randomNumber) {
+                setMessage(`Tebrikler! Doğru tahmin ettiniz.Toplam ${test+1} denemede bildiniz.`);
+            } else if (userGuess < randomNumber) {
+                setMessage("Daha yüksek bir sayı girin.");
+            } else {
+                setMessage("Daha düşük bir sayı girin.");
+            }
+        }
+    };
+
+    const handleRestart = () => {
+        setRandomNumber(generateRandomNumber());
+        setUserInput("");
+        setMessage("");
+        setTest(0);
     };
 
     return (
         <div className="game-container">
-            <button className="random-button" onClick={generateRandomNumber}>Random Sayı Üret</button> <br/>
-            <p className="show-number">{randomNumber}</p>
+            <h3>SAYI TAHMİN OYUNU</h3>
+
+            <input
+                className="form-field"
+                type="text"
+                value={userInput}
+                onChange={handleInputChange}
+                placeholder="Sayı tahmininizi girin"
+            />
+
+            <h3>{message}</h3>
+            <button className="random-button" onClick={handleGuess}>Tahmin et</button>
+            <button className="random-button" onClick={handleRestart}>Yeniden Başlat</button>
+
+            <br />
+            {/*<p className="show-number">{randomNumber}</p>*/}
         </div>
     );
 }
